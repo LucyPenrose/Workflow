6966ccfc5fe6d93558a9850040dccec66c6e77b5
Lucy <104632002+LucyPenrose@users.noreply.github.com>
2024-07-04 09:36:31 -0400

    Minor changes to scripts

---
+++ MainPageScripts/PostalCodes.js
@@ -0,0 +1,5 @@
+function togglePostalMenu() {
+    const menu = document.getElementById("Hidden-Postal-Codes"); 
+    menu.classList.toggle("Hidden-Postal-Codes-visible");
+    menu.classList.toggle("Hidden-Postal-Codes-hidden");
+}
